5b9f12f7b85aa606d6a743a55b2dcc9b88f223b3
HP <HP@LAPTOP-C6VLLB5K>
2020-02-08 13:56:27 +0530

    first commit

---
+++ maven.pro/src/test/java/maven/pro/NewyeargiftTest.java
@@ -0,0 +1,14 @@
+package maven.pro;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NewyeargiftTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
